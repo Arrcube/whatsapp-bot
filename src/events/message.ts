c88d09dde2c3cabe1c7c779e7ef5e9fed311f7f9
lutfisobri <lutfisobri@gmail.com>
2023-07-31 19:42:10 +0700

    add command help

--- src/events/message.ts
+++ src/events/message.ts
@@ -6,14 +6,27 @@ const prefix = process.env.PREFIX!;
 
 const message = (client: Client) => {
     client.on("message", async (message) => {
+        const command = message.body.split(" ")[0].replace(prefix, "");
+        const args = message.body.split(" ").slice(1);
+        
+        let listCommands: {
+            name: string;
+            description: string;
+            aliases: string[] | undefined;
+        }[] = [];
+
         const commandFiles = fs.readdirSync("./src/commands").filter((file) => file.endsWith(".ts"));
         for (const file of commandFiles) {
             const fileName = file.split(".")[0] + ".js";
             const commandModule = await import(`../commands/${fileName}`);
             const commands = commandModule.default;
 
-            const command = message.body.split(" ")[0].replace(prefix, "");
-            const args = message.body.split(" ").slice(1);
+            listCommands.push({
+                name: commands.name,
+                description: commands.description,
+                aliases: commands.aliases,
+            });
+
 
             if (message.body.startsWith(prefix)) {
                 if (commands.name === command) {
@@ -22,6 +35,14 @@ const message = (client: Client) => {
             } else if (commands.aliases?.includes(command)) {
                 commands.run(message, args);
             }
+        }
+
+        if (command === "help") {
+            let text = "*List Perintah Bot*\n";
+            listCommands.forEach((command) => {
+                text += `\n${prefix}${command.name}: ${command.description}`;
+            });
+            message.reply(text);
         }
     });
 }
